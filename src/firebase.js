5c160e5ee6fc9284489e1404133c7bb7e6888249
hsynarslan <hsyn06arslan@gmail.com>
2021-12-24 19:47:19 +0300

    env

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCSHkWH1mQF9dRqnEtem1Mv-LKTgdAakCE',
-  authDomain: 'todo-app-6a400.firebaseapp.com',
-  projectId: 'todo-app-6a400',
-  storageBucket: 'todo-app-6a400.appspot.com',
-  messagingSenderId: '635388393436',
-  appId: '1:635388393436:web:819f2b9331c2dabb938119'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = firebase.initializeApp(firebaseConfig);
